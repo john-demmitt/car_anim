562d2c412da23fdbc3cb3b0360762d22ce5d04ea
John Demmitt <john_demmitt@outlook.com>
2017-07-18 07:41:53 -0700

    working esv door anim

--- js/3danim.js
+++ js/3danim.js
@@ -5,17 +5,13 @@ jQuery(document).ready(function($) {
     var half = width/4;
     var rDoor = $("#rDoor");
     var esv = $("#esv");
-  
-   
+
 var tl = new TimelineMax();
    tl.to([fWheel, rWheel], 2, { rotation: -1800, transformOrigin: "50% 50%" }, "esv")
-    .fromTo(esv, 2, { x: width }, { x: half }, "esv")
-    .to(esv, 0.5, {autoAlpha:1}, "esv")
-    .fromTo(rDoor, 2, { x: width }, { x: half }, "esv");
-    
-    tl.to(rDoor, 1, {rotationY: -60, transformOrigin: "280 50% 1"});
-    
-   
-
+    .fromTo([esv, rDoor], 2, { x: width }, { x: half }, "esv")
+    .to([esv, rDoor], 0.5, {autoAlpha:1}, "esv")
+   .to(rDoor, 1, {rotationY: -60, transformOrigin: "260 50% -10"})
+   .to(rDoor, 1, {rotationY: 0, transformOrigin: "260 50% -10", delay:1})
+   .to([esv,rDoor], 2, { x: -width});
 
 });
