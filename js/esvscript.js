197abe3a5ab765bf034cbe91f696695462d76dd0
John Demmitt <john_demmitt@outlook.com>
2017-07-17 18:31:45 -0700

    play

--- js/esvscript.js
+++ js/esvscript.js
@@ -2,14 +2,17 @@ jQuery(document).ready(function($) {
     var width = $(document).width();
     var fWheel = $("#fWheel");
     var rWheel = $("#rWheel");
-    var half = width/2;
+    var half = width/4;
     var rDoor = $("#rDoor");
-    
+    var esv = $("#esv");
+    TweenLite.set($("svg"), {perspective:200});
    
 var tl = new TimelineMax();
-    tl.to([fWheel, rWheel], 4, { rotation: -6000, transformOrigin: "50% 50%" }, "esv")
-    .fromTo($("#esv"), 4, { x: width }, { x: half }, "esv")
-    .to(rDoor, 2, {rotationY: 60, transformOrigin: "left"});
+   tl.to([fWheel, rWheel], 2, { rotation: -1800, transformOrigin: "50% 50%" }, "esv")
+    .fromTo(esv, 2, { x: width }, { x: half }, "esv")
+    .to(esv, 0.5, {autoAlpha:1}, "esv");
+
+    tl.to(rDoor, 2, {rotationY: 180, transformOrigin: left});
     
    
 
