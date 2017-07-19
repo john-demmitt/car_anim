a87ff6b9159efbb2bef502b2b87ad18ef64caa99
John Demmitt <john_demmitt@outlook.com>
2017-07-18 21:56:11 -0700

    more stuff

--- js/3danim.js
+++ js/3danim.js
@@ -10,8 +10,8 @@ var tl = new TimelineMax();
    tl.to([fWheel, rWheel], 2, { rotation: -1800, transformOrigin: "50% 50%" }, "esv")
     .fromTo([esv, rDoor], 2, { x: width }, { x: half }, "esv")
     .to([esv, rDoor], 0.5, {autoAlpha:1}, "esv")
-   .to(rDoor, 1, {rotationY: -60, transformOrigin: "260 50% -10"})
-   .to(rDoor, 1, {rotationY: 0, transformOrigin: "260 50% -10", delay:1})
+   .to(rDoor, 1, {rotationY: -75, transformOrigin: "320 208 -10", ease: Power4.easeInOut })
+   .to(rDoor, 1, {rotationY: 0, transformOrigin: "320 208 -10", ease: Power4.easeInOut, delay:1})
    .to([esv,rDoor], 2, { x: -width});
 
 });
