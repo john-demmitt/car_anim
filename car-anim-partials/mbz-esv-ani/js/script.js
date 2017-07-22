abca19926a1eb1df743bae4efca346be101d1814
John Demmitt <john_demmitt@outlook.com>
2017-07-22 04:58:32 -0700

    sat am backup

--- car-anim-partials/mbz-esv-ani/js/script.js
+++ car-anim-partials/mbz-esv-ani/js/script.js
@@ -4,17 +4,20 @@ jQuery(document).ready(function($) {
     var rRim = $("#rRim");
     var fWheel = $("#fWheel");
     var rWheel = $("#rWheel");
-    var pauseBtn = $("#pause");
-    
+    var esv = $("#esv");
+    var mbz = $("#mbz");
+    var svg = $("svg");
+
     /*var rimsRot = TweenLite.to([fRim, rRim], 5, { rotation: 4800, transformOrigin: "50% 50%" });
     var mbzMove = TweenLite.fromTo($("#mbz"), 5, { x: "-400vw" }, { x: "400vw" });
     var wheelRot = TweenLite.to([fWheel, rWheel], 5, { rotation: -4800, transformOrigin: "50% 50%" });
     var esvMove = TweenLite.fromTo($("#esv"), 5, { x: "400vw" }, { x: "-400vw" });*/
 var tl = new TimelineMax();
-    tl.fromTo($("#mbz"), 8, {  x: -width }, {x: width}, "merc")
-    .to([fRim, rRim], 8, { rotation: 6000, transformOrigin: "50% 50%"}, "merc")
-    .to([fWheel, rWheel], 8, { rotation: -6000, transformOrigin: "50% 50%" }, "merc")
-    .fromTo($("#esv"), 8, { x: width }, { x: -width }, "merc");
+    tl.fromTo($("#mbz"), 4, {  x: -width }, {x: width}, "merc")
+    .to([fRim, rRim], 4, { rotation: 4000, transformOrigin: "50% 50%"}, "merc")
+    .to([fWheel, rWheel], 4, { rotation: -4000, transformOrigin: "50% 50%" }, "merc")
+    .fromTo($("#esv"), 4, { x: width }, { x: -width }, "merc")
+    .to(svg, 0.5, {autoAlpha:1}, "merc");
     
    /* rimsRot("mbz").
     mbzMove("mbz").
